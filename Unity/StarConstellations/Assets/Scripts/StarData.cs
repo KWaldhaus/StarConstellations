76bc5294f0fec45afb68b2bc28cf939920cd66ff
KWaldhaus <kilian.waldhaus@stud.fh-flensburg.de>
2020-10-15 18:21:50 +0200

    - added components for star

---
+++ Unity/StarConstellations/Assets/Scripts/StarData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using DefaultNamespace;
+
+public class StarData
+{
+    private List<StarDataStruct> _starDataStructs = new List<StarDataStruct>();
+    public void GetStars()
+    {
+        DataReader dataReader = new DataReader();
+        _starDataStructs = dataReader.ReadFile();
+    }
+}
